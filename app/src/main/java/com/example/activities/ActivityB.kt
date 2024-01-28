4b175db82e534cf964c957f1b4a6eeb7ad4d7171
Paxagi <paxagi@yandex.ru>
2024-01-28 23:41:34 +0300

    l3-task: add: add "B" and "C" activity, set other launch mode for activities

---
+++ app/src/main/java/com/example/activities/ActivityB.kt
@@ -0,0 +1,14 @@
+package com.example.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityB : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_b)
+
+        startActivity(Intent(this, ActivityC::class.java))
+    }
+}
