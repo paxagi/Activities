72b0f100dfa867a441dfd0f56eb5ee9fae4d0d4e
Paxagi <paxagi@yandex.ru>
2024-02-04 16:23:30 +0300

    l8-task: fix: move view-init function out of lifecycle function

--- app/src/main/java/com/example/activities/ActivityA.kt
+++ app/src/main/java/com/example/activities/ActivityA.kt
@@ -4,28 +4,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 
 class ActivityA : AppCompatActivity() {
+    private val name: String
+        get() = findViewById<EditText>(R.id.first_name).text.toString()
+
+    private fun showNameInToast(button: View) {
+        Toast.makeText(
+            this,
+            name,
+            Toast.LENGTH_SHORT
+        ).show()
+        Log.d("listener", "name: $name")
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("lifecycle", "onCreate: A")
+
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.btn_apply).setOnClickListener { showNameInToast(it) }
 
         startActivity(Intent(this, ActivityB::class.java))
-
-        findViewById<Button>(R.id.btn_apply).setOnClickListener {
-            val name = findViewById<EditText>(R.id.first_name).text.toString()
-            Toast.makeText(
-                this,
-                name,
-                Toast.LENGTH_SHORT
-            ).show()
-            Log.d("listener", "name: $name")
-        }
-
     }
 
     override fun onStart() {
