1fe7c3fdcb443c81e3e2a2068bf093c21f081efd
Paxagi <paxagi@gmail.com>
2024-02-07 22:24:57 +0300

    l8-task: add/ref: create "next activity" buttons and navigate by them, change priority of xml tags properties, remove nigth-string xml, delete showNameInToast function, state views as lazy properties

--- app/src/main/java/com/example/activities/ActivityB.kt
+++ app/src/main/java/com/example/activities/ActivityB.kt
@@ -4,22 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 
 class ActivityB : AppCompatActivity() {
 
-    lateinit var editDescription: EditText
+    private val editDescription: EditText by lazy {
+        findViewById<EditText>(R.id.editTextTextB)
+    }
+
+    private val btnNextActivity: Button by lazy {
+        findViewById<Button>(R.id.bnt_go_to_activityC)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d("lifecycle", "onCreate: B")
         setContentView(R.layout.activity_b)
-        editDescription = findViewById<EditText>(R.id.editTextTextB)
-
-        startActivityForResult(
-            Intent(this, ActivityC::class.java)
-                .putExtra("msg", "open C"),
-            0
-        )
+        btnNextActivity.setOnClickListener{
+            startActivityForResult(
+                Intent(this, ActivityC::class.java)
+                    .putExtra("msg", "open C"),
+                0
+            )
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
