4a54f5e174b7b0ef135c011ba2e9df76828cb8d6
Paxagi <paxagi@gmail.com>
2024-02-10 14:52:51 +0300

    l10-task: add: add 3 edit texts and listeners for them

--- app/src/main/java/com/example/activities/ActivityA.kt
+++ app/src/main/java/com/example/activities/ActivityA.kt
@@ -3,14 +3,21 @@ package com.example.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 
 class ActivityA : AppCompatActivity() {
-    private val name: String
+
+    private val city: EditText by lazy { findViewById(R.id.etCity) }
+    private val street: EditText by lazy { findViewById(R.id.etStreet) }
+    private val house: EditText by lazy { findViewById(R.id.etHouse) }
+
+private val name: String
         get() = findViewById<EditText>(R.id.first_name).text.toString()
 
     private fun showNameInToast(button: View) {
@@ -27,6 +34,10 @@ class ActivityA : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.btn_apply).setOnClickListener { showNameInToast(it) }
+        fun etLogD(text: Editable?) = Log.d("etListen", "text changed: $text")
+        city.addTextChangedListener { etLogD(it) }
+        street.addTextChangedListener { etLogD(it) }
+        house.addTextChangedListener { etLogD(it) }
 
         startActivity(Intent(this, ActivityB::class.java))
     }
