d6d43544d80a82f6e28bc3602835950f3b3abf20
Paxagi <paxagi@gmail.com>
2024-02-21 12:52:46 +0300

    l15-task: ref: use Parcelable Person

--- app/src/main/java/com/example/activities/Person.kt
+++ app/src/main/java/com/example/activities/Person.kt
@@ -1,10 +1,43 @@
 package com.example.activities
 
-import java.io.Serializable
+import android.os.Parcel
+import android.os.Parcelable
 
-data class Person (
-    val name: String,
-    val surname: String,
-    val birthday: String,
-    val country: String,
-) : Serializable
+data class Person(
+    val name: String?,
+    val surname: String?,
+    val birthday: String?,
+    val country: String?,
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+    ) {
+    }
+
+    override fun describeContents(): Int = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(name)
+        dest.writeString(surname)
+        dest.writeString(birthday)
+        dest.writeString(country)
+    }
+
+    companion object CREATOR : Parcelable.Creator<Person> {
+        override fun createFromParcel(parcel: Parcel): Person {
+            return Person(
+                name = parcel.readString(),
+                surname = parcel.readString(),
+                birthday = parcel.readString(),
+                country = parcel.readString(),
+            )
+        }
+
+        override fun newArray(size: Int): Array<Person?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
