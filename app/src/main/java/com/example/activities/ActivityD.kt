fcedab972cac87606513a17d5ac974d2fe708872
Paxagi <paxagi@yandex.ru>
2024-02-28 20:15:51 +0300

    l22-task: add: add activity D and update navigate between activity

---
+++ app/src/main/java/com/example/activities/ActivityD.kt
@@ -0,0 +1,15 @@
+package com.example.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+
+class ActivityD : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_d)
+    }
+
+    override fun onCreateOptionsMenu(menu: android.view.Menu?): Boolean = MenuListener().create(this, menu)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = MenuListener().itemSelected(this, item)
+}
